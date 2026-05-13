cea93c6186700e40984030043b145ffb920dcbf4
Vansh Gupta <vanshgupta7017@outlook.com>
2026-05-13 23:10:13 +0530

    daily streak lc -1674 completed

---
+++ dailystreak/LC-1674(13thMay26)/bruteforce.py
@@ -0,0 +1,39 @@
+﻿#gives tle dont try
+##time complexity: O(n * m) where n is the length of nums
+# and m is the range of possible sums (2*limit)  
+#space complexity: O(1) as we are using constant space
+
+
+class Solution:
+    def minMoves(self, nums: List[int], limit: int) -> int:
+        n = len(nums)
+        ans = float('inf')
+
+        for target in range(2, 2 * limit + 1):
+
+            moves = 0
+
+
+            for i in range(n // 2):
+
+                a = nums[i]
+                b = nums[n - 1 - i]
+
+                # 0 move
+                if a + b == target:
+                    continue
+
+                # 1 move possible
+                elif (1 <= target - a <= limit or
+                      1 <= target - b <= limit):
+
+                    moves += 1
+
+                # otherwise 2 moves
+                else:
+                    moves += 2
+
+            ans = min(ans, moves)
+
+        return ans
+         
---
+++ dailystreak/LC-1674(13thMay26)/optimal.py
@@ -0,0 +1,36 @@
+﻿#time complexity: O(n + m) where n is the length of nums and m is the range of possible sums (2*limit)
+#space complexity: O(m) where m is the range of possible sums (2*limit
+
+
+class Solution:
+    def minMoves(self, nums: List[int], limit: int) -> int:
+        n = len(nums)
+        diff = [0] * (2 * limit + 2)
+
+        # Initially every pair costs 2 moves
+        pairs = n // 2
+
+        for i in range(pairs):
+            a = nums[i]
+            b = nums[n - 1 - i]
+
+            low = min(a, b) + 1
+            high = max(a, b) + limit
+            s = a + b
+
+            # one move range
+            diff[low] -= 1
+            diff[high + 1] += 1
+
+            # exact sum -> one more reduction
+            diff[s] -= 1
+            diff[s + 1] += 1
+
+        ans = float('inf')
+        curr = pairs * 2
+
+        for target in range(2, 2 * limit + 1):
+            curr += diff[target]
+            ans = min(ans, curr)
+
+        return ans
