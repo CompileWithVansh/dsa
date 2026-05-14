69c7180575df9c16177fe2f535387f02f3b36f88
Vansh Gupta <vanshgupta7017@outlook.com>
2026-05-15 00:20:14 +0530

    daily streak lc-2874 completed

---
+++ dailystreak/LC-2874(15thMay26)/bruteforce.java
@@ -0,0 +1,25 @@
+﻿//time complexity: O(nlogn) due to sorting
+//space complexity: O(1)
+
+class Solution {
+    public boolean isGood(int[] nums) {
+        
+        Arrays.sort(nums);
+
+        int n = nums[nums.length - 1];
+
+        // Length should be n + 1
+        if (nums.length != n + 1) {
+            return false;
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] != i + 1) {
+                return false;
+            }
+        }
+
+        // Last two elements should be n
+        return nums[n - 1] == n && nums[n] == n;
+    }
+}
---
+++ dailystreak/LC-2874(15thMay26)/optimal.java
@@ -0,0 +1,72 @@
+﻿//time complexity: O(n)
+//space complexity: O(n)
+
+class Solution {
+    public boolean isGood(int[] nums) {
+
+        int n = nums.length - 1;
+
+        int[] freq = new int[n + 1];
+
+        for (int num : nums) {
+
+            // Number should be between 1 and n
+            if (num < 1 || num > n) {
+                return false;
+            }
+
+            freq[num]++;
+        }
+
+        // 1 to n-1 should appear exactly once
+        for (int i = 1; i < n; i++) {
+            if (freq[i] != 1) {
+                return false;
+            }
+        }
+
+        // n should appear exactly twice
+        return freq[n] == 2;
+    }
+}
+
+
+//try 2
+//time complexity: O(n)
+//space complexity: O(1)
+
+class Solution {
+    public boolean isGood(int[] nums) {
+
+        int n = nums.length - 1;
+
+        int duplicate = -1;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            int val = Math.abs(nums[i]);
+
+            // value must be between 1 and n
+            if (val < 1 || val > n) {
+                return false;
+            }
+
+            int idx = val - 1;
+
+            if (nums[idx] < 0) {
+
+                // only n can repeat
+                if (val != n || duplicate != -1) {
+                    return false;
+                }
+
+                duplicate = val;
+
+            } else {
+                nums[idx] *= -1;
+            }
+        }
+
+        return duplicate == n;
+    }
+}
