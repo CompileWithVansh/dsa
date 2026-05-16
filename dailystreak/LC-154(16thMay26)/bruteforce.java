ed01a57ac2e60bdc82b2281c5ea1974fdb31a6ee
Vansh Gupta <vanshgupta7017@outlook.com>
2026-05-17 00:01:10 +0530

    daily streak 154 leetcode tackled and completed easily

---
+++ dailystreak/LC-154(16thMay26)/bruteforce.java
@@ -0,0 +1,11 @@
+﻿//time complexity: O(n)
+//space complexity: O(1)
+class Solution {
+    public int findMin(int[] nums) {
+        int min = Integer.MAX_VALUE;
+        for (int num : nums) {
+            min = Math.min(min, num);
+        }
+        return min;
+    }
+}
---
+++ dailystreak/LC-154(16thMay26)/optimal.java
@@ -0,0 +1,20 @@
+﻿//time complexity: O(log n) in the average case, O(n) in the worst case (when there are duplicates)
+//space complexity: O(1)
+
+class Solution {
+    public int findMin(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else if (nums[mid] < nums[right]) {
+                right = mid;
+            } else {
+                right--;
+            }
+        }
+        return nums[left];
+    }
+}
