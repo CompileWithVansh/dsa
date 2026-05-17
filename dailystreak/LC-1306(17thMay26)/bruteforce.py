017e902d7477bc2c83f5fb2e4be3e752d85443fa
Vansh Gupta <vanshgupta7017@outlook.com>
2026-05-17 23:57:26 +0530

    \daily streak lc-1307 intresting game question

---
+++ dailystreak/LC-1306(17thMay26)/bruteforce.py
@@ -0,0 +1,52 @@
+﻿#time complexity: O(n)
+# space complexity: O(n)
+
+#approch create a graph and then do dfs to find the zero value
+#then we will mark the visited nodes to avoid cycles and infinite loops
+#then we will check if the current node is zero or not if it is zero then we will return true else we will continue to explore the neighbors of the current node
+
+class Solution:
+    def canReach(self, arr, start):
+
+        n = len(arr)
+
+        graph = {}
+
+        for i in range(n):
+
+            neighbors = []
+
+            forward = i + arr[i]
+            backward = i - arr[i]
+
+            # Valid forward jump
+            if forward < n:
+                neighbors.append(forward)
+
+            # Valid backward jump
+            if backward >= 0:
+                neighbors.append(backward)
+
+            graph[i] = neighbors
+
+        visited = set()
+
+        def dfs(node):
+
+            # Already visited
+            if node in visited:
+                return False
+            # Mark visited
+            visited.add(node)
+            # Found zero
+            if arr[node] == 0:
+                return True
+                
+            for neighbor in graph[node]:
+
+                if dfs(neighbor):
+                    return True
+
+            return False
+
+        return dfs(start)
---
+++ dailystreak/LC-1306(17thMay26)/optimal.py
@@ -0,0 +1,38 @@
+﻿#time complexity: O(n)
+#space complexity: O(n)
+#approch graph implicit traversal using dfs
+#NO EXTRA SPACE FOR GRAPH CONSTRUCTION 
+#SPACE COMPLEXITY IS O(N) DUE TO RECURSION STACK AND VISITED ARRAY BUT BETTER 
+#THAN THE BRUTE FORCE APPROACH WHICH ALSO USES O(N) SPACE FOR GRAPH CONSTRUCTION
+
+class Solution:
+    def canReach(self, arr, start):
+
+        visited = [False] * len(arr)
+
+        def dfs(index):
+
+            # Out of bounds
+            if index < 0 or index >= len(arr):
+                return False
+
+            # Already visited
+            if visited[index]:
+                return False
+
+            # Found 0
+            if arr[index] == 0:
+                return True
+
+            # Mark visited
+            visited[index] = True
+
+            # Move forward
+            forward = index + arr[index]
+
+            # Move backward
+            backward = index - arr[index]
+
+            return dfs(forward) or dfs(backward)
+
+        return dfs(start)
