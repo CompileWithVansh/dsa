9370837d537dfcb2a6d86377092a48e416014768
Vansh Gupta <vanshgupta7017@outlook.com>
2026-05-19 23:33:46 +0530

    dailystreak lc-2540 twopointer solved

---
+++ dailystreak/LC-2540(19thMay26)/bruteforce.java
@@ -0,0 +1,15 @@
+﻿//time complexity: O(m*n) where m and n are the lengths of the two input arrays
+//space complexity: O(1) as we are using only constant extra space
+//brute force approach: we iterate through each element of the first array and compare it
+class Solution {
+    public int getCommon(int[] nums1, int[] nums2) {
+        for (int i = 0; i < nums1.length; i++) {
+            for (int j = 0; j < nums2.length; j++) {
+                if (nums1[i] == nums2[j]) {
+                    return nums1[i];
+                }
+            }
+        }
+        return -1;
+    }
+}
---
+++ dailystreak/LC-2540(19thMay26)/optimal.java
@@ -0,0 +1,24 @@
+﻿//time complexity: O(m+n) where m and n are the lengths of the two input arrays
+//space complexity: O(1) as we are using only constant extra space
+//two pointer approach: we initialize two pointers, one for each array, and compare the elements at those pointers. 
+
+class Solution {
+    public int getCommon(int[] nums1, int[] nums2) {
+        int i = 0, j = 0;
+
+        while (i < nums1.length && j < nums2.length) {
+
+            if (nums1[i] == nums2[j]) {
+                return nums1[i];
+            }
+
+            if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+
+        return -1;
+    }
+}
