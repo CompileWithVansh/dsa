5e188fa08ce7e5581a90294779edb927ad70997c
Vansh Gupta <vanshgupta7017@outlook.com>
2026-05-20 23:24:49 +0530

    daily streak completed

---
+++ dailystreak/LC-2657(20thMay26)/bruteforce.py
@@ -0,0 +1,27 @@
+﻿#time complexity: O(n^2)
+#space complexity: O(n)
+
+
+
+class Solution:
+    def findThePrefixCommonArray(self, A: List[int], B: List[int]) -> List[int]:
+        n = len(A)
+        
+        setA = set()
+        setB = set()
+        
+        ans = []
+        
+        for i in range(n):
+            setA.add(A[i])
+            setB.add(B[i])
+            
+            count = 0
+            
+            for num in setA:
+                if num in setB:
+                    count += 1
+            
+            ans.append(count)
+        
+        return ans
---
+++ dailystreak/LC-2657(20thMay26)/optimal.py
@@ -0,0 +1,26 @@
+﻿#time complexity: O(n)
+#space complexity: O(n)
+
+class Solution:
+    def findThePrefixCommonArray(self, A: List[int], B: List[int]) -> List[int]:
+        
+        n = len(A)
+
+        visited = [0] * (n + 1)
+
+        ans = []
+        common = 0
+
+        for i in range(n):
+
+            visited[A[i]] += 1
+            if visited[A[i]] == 2:
+                common += 1
+
+            visited[B[i]] += 1
+            if visited[B[i]] == 2:
+                common += 1
+
+            ans.append(common)
+
+        return ans
