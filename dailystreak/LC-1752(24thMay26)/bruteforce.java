8fffbc0d0c2fc0ba78cf41f61a26acf51ee5b4f0
Vansh Gupta <vanshgupta7017@outlook.com>
2026-05-24 00:52:41 +0530

    checked array is sorted and rotated dailystreak

---
+++ dailystreak/LC-1752(24thMay26)/bruteforce.java
@@ -0,0 +1,12 @@
+﻿// Brute Force Idea
+
+Try every possible rotation.
+
+For each rotation:
+
+Rotate array
+Check if rotated array becomes sorted
+
+If any rotation becomes sorted → return true.
+
+Otherwise false.
---
+++ dailystreak/LC-1752(24thMay26)/optimal.java
@@ -0,0 +1,21 @@
+﻿//time complexity: O(n)
+//space complexity: O(1)
+
+
+class Solution {
+    public boolean check(int[] nums) {
+    
+        int n = nums.length;
+        int count = 0;
+
+        for(int i = 0; i < n; i++) {
+
+            if(nums[i] > nums[(i + 1) % n]) {
+                count++;
+            }
+
+        }
+
+        return count <= 1;
+    }
+}
