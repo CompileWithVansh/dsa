00a776c5738d3d2f3ab13736890904426bd2a8b4
Vansh Gupta <vanshgupta7017@outlook.com>
2026-05-25 23:43:19 +0530

    daily streak completed

---
+++ dailystreak/LC-1871(25thMay26)/bruteforce.py
@@ -0,0 +1,27 @@
+﻿#time complexity: O(n^2)
+#space complexity: O(n)
+
+
+class Solution:
+    def canReach(self, s: str, minJump: int, maxJump: int) -> bool:
+
+        n = len(s)
+        visited = set()
+
+        def dfs(i):
+
+            if i == n - 1:
+                return True
+
+            visited.add(i)
+
+            for j in range(i + minJump, min(i + maxJump + 1, n)):
+
+                if s[j] == '0' and j not in visited:
+
+                    if dfs(j):
+                        return True
+
+            return False
+
+        return dfs(0)
